ecf65b854e61c3bfb61b8f10cdd473c70f2d885b
Vanshika-Atiuttam <Vanshika@atiuttam.tech>
2023-04-17 00:47:07 +0530

    first commit

---
+++ my-app/src/pages/home/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./home.css"
+
+
+function Home() {
+  return (
+    <div>
+        <div className='hero-outer-div'>
+
+        <img src="/images/hero.png" alt="" className='hero-img'/>
+        <div className='hero-h1-div'>
+
+        <h1 className='hero-h1-1'>
+        All in One
+        </h1>
+        <h1 className='hero-h1-2'>
+           Guide to get you placed!
+        </h1>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
