a33d35d7ce683ab5bc64fdab2f61112d9473c041
chanpreetkaur20 <chanpreetkaur2005@gmail.com>
2023-04-17 22:44:47 +0530

    added navbar

--- my-app/src/routes/routes.jsx
+++ my-app/src/routes/routes.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from '../components/header/Header';
 import Homepage from "../pages/home/Home"
 function routes() {
   return (
     <Router>
+    <Header/>  
       
     <Routes>
       <Route path="/" element={<Homepage />} />
